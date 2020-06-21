9b314e635922c915de8b794e7afd2689571d6333
LiangDennis <18898513421@163.com>
2020-06-22 00:03:00 +0800

    cluster features

--- README.md
+++ README.md
@@ -21,8 +21,37 @@ npm run build --report
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
 
-## 初始化项目 配置eslint 自动修复的功能
+### 初始化项目 配置eslint 自动修复的功能
 
-## 循环显示所有路由
+### 循环显示所有路由
 
-## 创建第一个openlayers的vue文件
+### 创建第一个openlayers的vue文件
+  #### 通过npm镜像下载openlayers@4.6.5版本
+    1. 引入ol.css 通过 import 'openlayers/dist/ol.css' 的方式
+    2. 通过 引入import ol from 'openlayers'，在代码中使用ol.xxx.yyy的方式使用openlayers中的各个组件
+    3. 在使用一个方法创建地图中，如果要获取features内容
+       1. 可以在创建的聚合中clusterSource 通过 .getSource().getFeatures() 的方式获取
+       2. 而当clusterSource 作为source成为图层layer的数据源时，此时通过创建的图层cluster.getSource().getSource().getFeatures()的方式获取到数据
+    4. 可以通过各个方法间的调用了创建
+
+### 创建feature的方式
+  1. new ol.Feature(new ol.geom.Point(data))
+     1. 此处的 data 是一个包含两个数据的数组，但是不是坐标，而是相对于[0, 0]点的位置距离
+     2. 此处的feature是通过一个一个地循环创建的feature
+     3. 思考：只能通过for循环一个一个创建吗？
+
+### 创建聚合的方式
+  1. 先拿到features的data数据
+  2. 使用矢量的source创建feature的source数据
+    <code>let source = new ol.source.Vector({ features: featuresData})</code>
+  3. 使用cluster 创建聚合
+    <code>let clusters = new ol.source.Cluster({
+      distance: 10,
+      source: source
+    })</code>
+  4. 思考：
+     1. 为什么要使用styleCache换成feature聚合的size大小？
+     2. 为什么在layer中使用style，而不是在Cluster中使用style
+     3. style的使用场景，使用方式
+
+#### 刚学真的是各种写错，写漏api的字母
--- src/components/ol-01.vue
+++ src/components/ol-01.vue
@@ -1,19 +1,193 @@
 <template>
   <div class="container">
     <h2>{{title}}</h2>
+    <input type="number" v-model="distance">
+    <div class="map" id="map"></div>
   </div>
 </template>
 
 <script>
+import 'openlayers/dist/ol.css'
+import ol from 'openlayers'
 export default {
   data () {
     return {
-      title: 'openlayers'
+      title: 'openlayers',
+      distance: 10,
+      styleCache: {},
+      map: null
+    }
+  },
+  mounted () {
+    this.createMap()
+
+    // this.onlyOneMethodToCreateMap()
+  },
+  methods: {
+    // 创建 feature 数据
+    createFeatureData () {
+      let count = 20
+      let e = 4500000
+      let feature = new Array(count)
+      for (let i = 0; i < count; i++) {
+        // for 循环一个个创建feature
+        // Point 接收的数据是一个包含两个数值得数组，同时这两个数值是相对于[0, 0]点的位置距离
+        const coordinates = [2 * e * Math.random() - e, 2 * e * Math.random() - e]
+        // console.log(coordinates)
+        feature[i] = new ol.Feature(new ol.geom.Point(coordinates))
+      }
+      return feature
+    },
+    // 创建 cluster 聚合数据
+    createClusterData () {
+      let features = this.createFeatureData()
+
+      let that = this
+      let source = new ol.source.Vector({
+        features: features
+      })
+
+      let clusterSource = new ol.source.Cluster({
+        distance: parseInt(that.distance, 10), // 聚合范围，同时将其转换成十进制
+        source: source
+      })
+
+      let clusters = new ol.layer.Vector({
+        source: clusterSource,
+        style: function (feature) {
+          let size = feature.get('features').length
+          let style = that.styleCache[size]
+          if (!style) {
+            style = new ol.style.Style({ // 创建每个feature的样式
+              image: new ol.style.Circle({ // 图片是一个openlayers原生的圆
+                radius: 10, // 圆的半径
+                stroke: new ol.style.Stroke({ // 这个圆的边
+                  color: '#f00'// 圆的边的颜色
+                }),
+                fill: new ol.style.Fill({ // 对这个圆进行填充
+                  color: '#fff' // 填充的颜色
+                })
+              }),
+              text: new ol.style.Text({ // 修改字体的样式
+                text: size.toString(), // size表示聚合后圆包括的feature的个数
+                fill: new ol.style.Fill({ // 字体的填充颜色
+                  color: '#000'
+                })
+              })
+            })
+            that.styleCache[size] = style
+          }
+          return style
+        }
+      })
+
+      return clusters
+    },
+    // 创建地图
+    createMap () {
+      let clusters = this.createClusterData()
+
+      let raster = new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+
+      this.map = new ol.Map({
+        layers: [raster, clusters],
+        target: 'map',
+        view: new ol.View({
+          // center: [121.3028308866, 31.0164689898],
+          // projection: 'EPSG:4326',
+          center: [0, 0],
+          zoom: 2
+        })
+      })
+      // let raster = new ol.layer.Tile({
+      //   source: new ol.source.OSM()
+      // })
+
+      // this.map = new ol.Map({
+      //   layers: [raster],
+      //   target: 'map',
+      //   view: new ol.View({
+      //     center: [121.3028308866, 31.0164689898],
+      //     projection: 'EPSG:4326', // 在view层使用这个，可以将地图中0，0的位置方式换成是地图坐标的方式
+      //     zoom: 10
+      //   })
+      // })
+    },
+    onlyOneMethodToCreateMap () {
+      let count = 20
+      let features = new Array(count)
+      let e = 4500000
+      for (let i = 0; i < count; ++i) {
+        let coordinates = [2 * e * Math.random() - e, 2 * e * Math.random() - e]
+        features[i] = new ol.Feature(new ol.geom.Point(coordinates))
+      }
+
+      let source = new ol.source.Vector({
+        features: features
+      })
+
+      let clusterSource = new ol.source.Cluster({
+        distance: parseInt(this.distance, 10),
+        source: source
+      })
+
+      // console.log(clusterSource.getSource().getFeatures())
+
+      let styleCache = {}
+      let clusters = new ol.layer.Vector({
+        source: clusterSource,
+        style: function (feature) {
+          let size = feature.get('features').length
+          let style = styleCache[size]
+          if (!style) {
+            style = new ol.style.Style({
+              image: new ol.style.Circle({
+                radius: 10,
+                stroke: new ol.style.Stroke({
+                  color: '#f00'
+                }),
+                fill: new ol.style.Fill({
+                  color: '#3399CC'
+                })
+              }),
+              text: new ol.style.Text({
+                text: size.toString(),
+                fill: new ol.style.Fill({
+                  color: '#fff'
+                })
+              })
+            })
+            styleCache[size] = style
+          }
+          return style
+        }
+      })
+
+      console.log(clusters.getSource().getSource().getFeatures())
+
+      let raster = new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+
+      this.map = new ol.Map({
+        layers: [raster, clusters],
+        target: 'map',
+        view: new ol.View({
+          center: [0, 0],
+          zoom: 2
+        })
+      })
+      // this.map = Object.freeze(map)
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped >
+.map{
+  width: 100%;
+  height: 400px;
+}
 </style>
