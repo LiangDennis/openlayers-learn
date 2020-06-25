d8b6d3550391f0653426be100b960ab97b7647f9
LiangDennis <18898513421@163.com>
2020-06-26 02:41:15 +0800

    modify, draw, snap, 以及获取feature的坐标的一些方式

--- README.md
+++ README.md
@@ -34,24 +34,68 @@ For a detailed explanation on how things work, check out the [guide](http://vuej
        2. 而当clusterSource 作为source成为图层layer的数据源时，此时通过创建的图层cluster.getSource().getSource().getFeatures()的方式获取到数据
     4. 可以通过各个方法间的调用了创建
 
-### 创建feature的方式
-  1. new ol.Feature(new ol.geom.Point(data))
-     1. 此处的 data 是一个包含两个数据的数组，但是不是坐标，而是相对于[0, 0]点的位置距离
-     2. 此处的feature是通过一个一个地循环创建的feature
-     3. 思考：只能通过for循环一个一个创建吗？
+  #### 创建feature的方式
+    1. new ol.Feature(new ol.geom.Point(data))
+      1. 此处的 data 是一个包含两个数据的数组，但是不是坐标，而是相对于[0, 0]点的位置距离
+      2. 此处的feature是通过一个一个地循环创建的feature
+      3. 思考：只能通过for循环一个一个创建吗？
 
-### 创建聚合的方式
-  1. 先拿到features的data数据
-  2. 使用矢量的source创建feature的source数据
-    <code>let source = new ol.source.Vector({ features: featuresData})</code>
-  3. 使用cluster 创建聚合
-    <code>let clusters = new ol.source.Cluster({
-      distance: 10,
-      source: source
-    })</code>
-  4. 思考：
-     1. 为什么要使用styleCache换成feature聚合的size大小？
-     2. 为什么在layer中使用style，而不是在Cluster中使用style
-     3. style的使用场景，使用方式
+  #### 创建聚合的方式
+    1. 先拿到features的data数据
+    2. 使用矢量的source创建feature的source数据
+      <code>let source = new ol.source.Vector({ features: featuresData})</code>
+    3. 使用cluster 创建聚合
+      <code>let clusters = new ol.source.Cluster({
+        distance: 10,
+        source: source
+      })</code>
+    4. 思考：
+      1. 为什么要使用styleCache换成feature聚合的size大小？
+      2. 为什么在layer中使用style，而不是在Cluster中使用style
+      3. style的使用场景，使用方式
 
-#### 刚学真的是各种写错，写漏api的字母
+### 第二个openlayers 地图的交互 draw，modify，snap
+  #### modify 对象，可以将draw出来后的feature进行拓展，也就是可以再修改
+    1. modify对象在创建地图后就可以创建
+       1. let modify = new ol.interaction.Modify({source: mySource})
+       2. 此处的mySource与地图中的source是同一个对象
+    2. 并添加到地图中，添加到地图的方式是使用地图的this.map.addInteraction(modify)的方式
+    3. 思考：为什么这个对象只需要创建一遍呢？
+  #### draw对象，点击地图根据相应的规则产生相应的feature
+    1. 创建的方式  使用vue的方式，这里缓存draw是需要更换feature类型的时候可以清楚这个draw，产生新的形式的draw
+       1. this.draw = new ol.interaction.Draw({
+         source: mySource, // 也是地图使用的source
+         type: feature.type, // feature的类型，主要有：Point, LineString, Polygon, Circle
+         style: new ol.style.Style({
+           stroke: new ol.style.Stroke({
+             color: // 设置draw时的颜色，线条的颜色，和圆与多边形的线条颜色
+             width: // 线条的宽度
+           }),
+           image: new ol.style.Circle({ // 在draw时圆的半径，或者说是鼠标上的draw point 指示的样式
+             radius: // 圆的半径
+             fill: new style.Fill({
+               color: 圆的填充颜色
+             })
+           })
+         })
+       })
+    2. 添加到地图中 this.map.addInteraction(this.draw)
+    3. 思考：draw中设置style的Fill样式是怎么样的呢？有哪些地方能够体现这个属性呢？
+  #### snap 对象 设置鼠标的自动吸附功能
+    1. 创建方式 this.snap = new ol.interaction.Snap({source: mySource}) // 缓存起来，以便清除
+    2. 添加到地图中 this.map.addInteraction(this.snap)
+    3. 思考：
+       1. 为什么这个snap对象需要使用缓存起来，当改变feature的类型的时候，需要将它清除，然后使用一个新的snap呢？
+       2. 为什么不是与modify对象那样，创建一次，添加一次就可以了呢?
+
+  #### feature 的坐标获取
+    1. 通过点击事件获取feature
+       1. 方法this.map.on('click', (e) => {
+         this.map.forEachFeatureAtPixel(e.pixel, () =>  {
+           // 通过这种方式获取feature元素
+         })
+       })
+       2. 这种方式获取的feature是一个个的，而不是数组
+       3. 如果地图点击上方有多个feature，那么就会一个一个的获取到所有的feature
+
+### 刚学真的是各种写错，写漏api的字母
--- src/components/ol-02.vue
+++ src/components/ol-02.vue
@@ -2,15 +2,142 @@
   <div class="container">
     <h2>{{title}}</h2>
     <div id="map" class="map"></div>
+    <select id="type" v-model="selectType" @change="handleTypeChange">
+        <option value="Point">Point</option>
+        <option value="LineString">LineString</option>
+        <option value="Polygon">Polygon</option>
+        <option value="Circle">Circle</option>
+      </select>
   </div>
 </template>
 
 <script>
+import 'openlayers/dist/ol.css'
+import ol from 'openlayers'
 export default {
   data () {
     return {
-      title: 'openlayers 可以移动的features'
+      title: 'openlayers 可以移动的features',
+      map: null,
+      draw: null,
+      snap: null,
+      selectType: 'Point'
+    }
+  },
+  mounted () {
+    this.createMap()
+    this.map.on('click', (e) => {
+      // console.log('地图被点击了')
+      // console.log(e)
+      this.map.forEachFeatureAtPixel(e.pixel, (feature) => {
+        console.log(feature.getGeometry().A)
+      })
+      // this.map.forEachLayerAtPixel(e.pixel, (layer) => {
+      //   console.log(layer)
+      // })
+    })
+  },
+  methods: {
+    handleTypeChange () {
+      this.map.removeInteraction(this.draw) // 移除旧的draw和snap
+      this.map.removeInteraction(this.snap)
+      this.createDrawSnap() // 创建新的draw和snap
+      // console.log(this.snap.getMap().getLayers()) // 多个layer
+      // 这个获取到的是Vector对象，同时draw和snap所使用的source都是这个，所以可以获取到features
+      // console.log(this.snap.getMap().getLayers().a[1].getSource().getFeatures()) // features数组
+      // 获取到的Layer是Tile对象，其source是OSM对象，并不能获取feature
+      // console.log(this.draw.getMap().getLayers().a[0].getSource())
+      // let features = this.snap.getMap().getLayers().a[1].getSource().getFeatures()
+      // features.forEach(v => {
+      //   console.log(v.N)
+      //   console.log(v.getGeometry().A)
+      //   console.log(v.getGeometry().getProperties())
+      // })
+    },
+    createMap () {
+      let raster = new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+
+      this.createSource()
+      let vector = this.createLayer()
+
+      this.map = new ol.Map({
+        layers: [raster, vector],
+        target: 'map',
+        view: new ol.View({
+          center: [121.3028308866, 31.0164689898],
+          projection: 'EPSG:4326',
+          // center: [0, 0],
+          zoom: 10
+        })
+      })
+      // this.createModify()
+      this.createDrawSnap()
+    },
+    createSource () {
+      this.source = new ol.source.Vector()
+    },
+    createLayer () {
+      let source = this.source
+      let vector = new ol.layer.Vector({
+        source: source,
+        style: new ol.style.Style({ // 填充的颜色，圆或者多边形的内部填充颜色
+          fill: new ol.style.Fill({
+            color: 'rgba(255, 0, 0, 0.2)'
+          }),
+          stroke: new ol.style.Stroke({ // 线条的样式，和圆和多边形的线条样式
+            // color: '#ffcc33',
+            color: 'rgb(0, 153, 255)',
+            width: 10
+          }),
+          image: new ol.style.Circle({ // 画出Point的圆的样式
+            radius: 50,
+            fill: new ol.style.Fill({
+              color: '#ffcc33'
+            })
+          })
+        })
+      })
+      return vector
+    },
+    createModify () {
+      let source = this.source
+      let modify = new ol.interaction.Modify({source: source}) // 设置线条可以再次被设置
+      this.map.addInteraction(modify)
+    },
+    createDrawSnap () {
+      let that = this
+      this.draw = new ol.interaction.Draw({
+        source: that.source,
+        type: that.selectType,
+        // 设置draw的时候使用的样式
+        style: new ol.style.Style({
+          // fill: new ol.style.Fill({
+          //   color: '#ff0000'
+          // }),
+          stroke: new ol.style.Stroke({ // 设置draw时的线条或者点的颜色
+            color: '#0000ff',
+            width: 10
+          }),
+          image: new ol.style.Circle({ // 设置鼠标的draw标记
+            radius: 20,
+            fill: new ol.style.Fill({
+              color: '#0000ff'
+            })
+          })
+        })
+      })
+      this.map.addInteraction(this.draw)
+      this.snap = new ol.interaction.Snap({source: that.source}) // 鼠标的自动吸附
+      this.map.addInteraction(this.snap)
     }
   }
 }
 </script>
+
+<style scoped>
+#type {
+  margin: 20px 0;
+}
+</style>
